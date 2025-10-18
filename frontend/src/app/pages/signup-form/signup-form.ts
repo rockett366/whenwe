ecdb8d7474801064f7e863b732feb3e2b57be09d
tobeyligaya <tobeyligaya2@csus.edu>
2025-10-18 10:47:09 -0700

    Add preferences field to signup form
    Introduces a text area for users to enter scheduling or lifestyle preferences in the signup form. Refactors priority selection to use a ratings array and updates styling and routing for improved user experience.

--- frontend/src/app/pages/signup-form/signup-form.ts
+++ frontend/src/app/pages/signup-form/signup-form.ts
@@ -3,35 +3,35 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router'; //
+
+interface Rating{
+  value: number;
+  viewValue: number;
+}
 
 @Component({
   selector: 'app-signup-form',
   standalone: true,
-  imports: [MatSelectModule, MatFormFieldModule, MatButtonModule, FormsModule],
+  imports: [RouterLink, MatInputModule, MatSelectModule, MatFormFieldModule, MatButtonModule, FormsModule],
   templateUrl: './signup-form.html',
   styleUrl: './signup-form.css'
 })
 export class SignupForm {
-  friends: number | null = null;
-  family: number | null = null;
-  school: number | null = null;
-  work: number | null = null;
-  self: number | null = null;
+  friends: number = 0;
+  family: number = 0;
+  school: number = 0;
+  work: number = 0;
+  self: number = 0;
+  preferences: string = "";
 
-  numbers = [1, 2, 3, 4, 5];
-
-  submitPriorities() {
-    const userPriorities = {
-      friends: this.friends,
-      family: this.family,
-      school: this.school,
-      work: this.work,
-      self: this.self
-    };
-    console.log('User priorities:', userPriorities);
-
-    // TODO: send to backend or navigate to dashboard
-  }
-
+  ratings: Rating[] = [
+    {value: 1, viewValue: 1},
+    {value: 2, viewValue: 2},
+    {value: 3, viewValue: 3},
+    {value: 4, viewValue: 4},
+    {value: 5, viewValue: 5}
+  ]
 
 }
