77936e33fda2a09a5069692c4b278f53d28e3a66
Rockett <eemejia366@gmail.com>
2025-10-18 15:11:39 -0700

    AI working: searches for the information requested out name, start_time

--- backend/app/ai_scheduler.py
+++ backend/app/ai_scheduler.py
@@ -1,12 +1,25 @@
 from openai import OpenAI
+from pydantic import BaseModel
 
 client = OpenAI() 
+class CalendarEvent(BaseModel):
+    name: str
+    start_time: str
+    end_time: str
+    participants: list[str]
 
-
+# def suggest_meeting_time(rank1, pref1, new_event1, calender_event1, rank2, pref2, new_event2, calender_event2):
 def suggest_meeting_time():
-    response = client.responses.create(
+    response = client.responses.parse(
         model="gpt-5-mini",
-        input="Write hi"
+        input=[
+            {"role": "system", "content": "Extract the event information."},
+            {
+                "role": "user",
+                "content": "Alice and Bob are going to a science fair on friday at 5pm, events an hour"
+            },
+        ],
+        text_format = CalendarEvent,
     )
-    return(response.output_text)
+    return(response.output_parsed)
 
